9867e8a7b675d6603f99f04c6bfd1545fbb88cd7
Thiago Felipe <thiagof.g3@gmail.com>
2020-06-14 04:23:54 -0300

    Criando controlle e rota

---
+++ app/Http/Controllers/ParceiroController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service\ParceiroService;
+use Illuminate\Http\Request;
+
+class ParceiroController extends Controller
+{
+    public $parceiroService;
+
+    public function __construct(ParceiroService $parceiroService)
+    {
+        $this->parceiroService = $parceiroService;        
+    }
+    
+    public function index(Request $request)
+    {
+        return $this->parceiroService->index($request);
+    }
+}
